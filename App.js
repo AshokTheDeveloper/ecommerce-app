eb7d87cf71efcea3798c3de54f60db9b35403afa
AshokTheDeveloper <ashokchodipalli1>
2024-01-02 14:51:41 +0000

    app.js file added initially

---
+++ App.js
@@ -0,0 +1,114 @@
+import {Component} from 'react'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    const productObj = cartList.find(eachItem => eachItem.id === product.id)
+
+    if (productObj) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachItem => {
+          if (productObj.id === eachItem.id) {
+            const updatedQuantity = eachItem.quantity + 1
+            return {...eachItem, quantity: updatedQuantity}
+          }
+          return eachItem
+        }),
+      }))
+    } else {
+      this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    }
+  }
+
+  deleteCartItem = id => {
+    const {cartList} = this.state
+    const remainingCartItems = cartList.filter(
+      eachCartItem => eachCartItem.id !== id,
+    )
+    this.setState({cartList: remainingCartItems})
+  }
+
+  removeAll = () => {
+    this.setState({cartList: []})
+  }
+
+  incrementQuantity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachItem => {
+        if (id === eachItem.id) {
+          const updatedQuantity = eachItem.quantity + 1
+          return {...eachItem, quantity: updatedQuantity}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  decrementQuantity = id => {
+    const {cartList} = this.state
+    const currentProduct = cartList.find(eachItem => id === eachItem.id)
+    if (currentProduct.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachItem => {
+          if (id === eachItem.id) {
+            const updatedQuantity = eachItem.quantity - 1
+            return {...eachItem, quantity: updatedQuantity}
+          }
+          return eachItem
+        }),
+      }))
+    }
+  }
+
+  render() {
+    const {cartList, quantity} = this.state
+
+    return (
+      <BrowserRouter>
+        <CartContext.Provider
+          value={{
+            cartList,
+            addCartItem: this.addCartItem,
+            deleteCartItem: this.deleteCartItem,
+            removeAll: this.removeAll,
+            quantity,
+            incrementQuantity: this.incrementQuantity,
+            decrementQuantity: this.decrementQuantity,
+          }}
+        >
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/products" component={Products} />
+            <ProtectedRoute
+              exact
+              path="/products/:id"
+              component={ProductItemDetails}
+            />
+            <ProtectedRoute exact path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="not-found" />
+          </Switch>
+        </CartContext.Provider>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
